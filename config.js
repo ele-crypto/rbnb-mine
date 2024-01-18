bc0012ea74c4d7507f54501e089b061769a90734
ele <98563772+ele-crypto@users.noreply.github.com>
2024-01-18 16:20:33 -0500

    Create config.js

---
+++ config.js
@@ -0,0 +1,9 @@
+const config = {
+  difficulty: '0x999999',
+  tick: 'rBNB',
+  walletTablePath: 'wallets.csv',
+  rpcUrl: 'https://bsc.rpc.blxrbdn.com',
+  responsePath: 'https://ec2-18-218-197-117.us-east-2.compute.amazonaws.com',
+}
+
+module.exports = config
