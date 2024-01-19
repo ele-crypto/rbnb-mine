d5cccab223ae6be35f9a165dee84e3b95ea3955e
ele <98563772+ele-crypto@users.noreply.github.com>
2024-01-19 16:50:47 -0500

    Update config.js

--- config.js
+++ config.js
@@ -1,10 +1,10 @@
-//https://bsc.rpc.blxrbdn.com
+// https://bsc-dataseed.bnbchain.org
 
 const config = {
   difficulty: '0x999999',
   tick: 'rBNB',
   walletTablePath: 'wallets.csv',
-  rpcUrl: 'https://bsc-dataseed.bnbchain.org',
+  rpcUrl: 'https://bsc.rpc.blxrbdn.com',
   responsePath: 'https://ec2-18-218-197-117.us-east-2.compute.amazonaws.com',
 }
 
