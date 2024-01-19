1deade133190eb55de43d428d4136d7956356410
ele <98563772+ele-crypto@users.noreply.github.com>
2024-01-19 16:32:05 -0500

    Update index.js

--- index.js
+++ index.js
@@ -1,31 +1,27 @@
-const { ethers } = require('ethers')
-const csv = require('fast-csv')
-const fs = require('fs')
+const { ethers } = require('ethers');
+const csv = require('fast-csv');
+const fs = require('fs');
 
-const { difficulty, walletTablePath, tick } = require('./config')
-const { postResultData, getRandomInt, sleepMS } = require('./lib')
+const { difficulty, walletTablePath, tick } = require('./config');
+const { postResultData, sleepMS } = require('./lib');
 
-process.env.NODE_TLS_REJECT_UNAUTHORIZED = '0'
+process.env.NODE_TLS_REJECT_UNAUTHORIZED = '0';
 
-// const provider = new ethers.providers.JsonRpcProvider(rpcUrl)
-
-const currentChallenge = ethers.utils.formatBytes32String(tick)
+const currentChallenge = ethers.utils.formatBytes32String(tick);
 
 // Find possible solutions
 function findSolution(difficulty, walletInfo) {
-  const { address } = walletInfo
-  while (1) {
-    const random_value = ethers.utils.randomBytes(32)
-    const potential_solution = ethers.utils.hexlify(random_value)
-    const hashed_solution = ethers.utils.keccak256(
-      ethers.utils.defaultAbiCoder.encode(
-        ['bytes32', 'bytes32', 'address'],
-        [potential_solution, currentChallenge, address],
-      ),
-    )
-    if (hashed_solution.startsWith(difficulty)) {
-      return potential_solution
-    }
+  const { address } = walletInfo;
+  const randomValue = ethers.utils.randomBytes(32);
+  const potentialSolution = ethers.utils.hexlify(randomValue);
+  const hashedSolution = ethers.utils.keccak256(
+    ethers.utils.defaultAbiCoder.encode(['bytes32', 'bytes32', 'address'], [potentialSolution, currentChallenge, address]),
+  );
+
+  if (hashedSolution.startsWith(difficulty)) {
+    return potentialSolution;
+  } else {
+    return null;
   }
 }
 
@@ -43,9 +39,26 @@ async function sendTransaction(solution, walletInfo) {
   await postResultData(JSON.stringify(body))
 }
 
-const initWallet = async () => {
-  const wallets = []
+// Function to send transactions concurrently
+async function sendTransactions(solutions, wallets) {
+  const transactionPromises = solutions.map(async (solution, index) => {
+    try {
+      const walletInfo = wallets[index];
+      await sendTransaction(solution, walletInfo);
+      console.log(`Sent successfully solution: ${solution} for wallet: ${walletInfo.address}`);
+    } catch (error) {
+      console.error(`Error sending solution for wallet: ${wallets[index].address}`);
+      console.error(error);
+    }
+  });
+
+  await Promise.all(transactionPromises);
+}
+
+// Initialize the wallets
+async function initWallet() {
   return new Promise((resolve, reject) => {
+    const wallets = [];
     fs.createReadStream(walletTablePath)
       .pipe(csv.parse({ headers: true }))
       .on('error', error => reject(error))
@@ -53,32 +66,42 @@ const initWallet = async () => {
         wallets.push({
           address: row['address'],
           privateKey: row['key'],
-        })
+        });
       })
-      .on('end', () => resolve(wallets))
-  })
+      .on('end', () => resolve(wallets));
+  });
 }
 
+// Principal function
 async function main() {
-  const wallets = await initWallet()
+  const wallets = await initWallet();
+
   try {
     while (true) {
-      const index = getRandomInt(0, wallets.length - 1)
-      const walletInfo = wallets[index]
-      const solution = findSolution(difficulty, walletInfo)
+      
+      // Promise map to run findSolution for all wallets simultaneously
+      const solutionsPromises = wallets.map(walletInfo => findSolution(difficulty, walletInfo));
+      const solutions = await Promise.all(solutionsPromises);
 
-      await sendTransaction(solution, walletInfo)
-      console.log(`Sent successfully solution: ${solution}`)
+      // Filter valid solutions to search again immediately
+      const validSolutions = solutions.filter(solution => solution !== null);
 
-      await sleepMS(50)
+      if (validSolutions.length > 0) {
+        console.log(`Sent successfully solutions: ${validSolutions}`);
+      }
+
+      // Send transactions concurrently
+      await sendTransactions(validSolutions, wallets);
+
+      await sleepMS(50);
     }
   } catch (err) {
-    console.log('Error ------------------')
-    console.log(err)
-    console.log('-----------------------')
-    console.log('Restart the program')
-    main()
+    console.log('Error ------------------');
+    console.log(err);
+    console.log('-----------------------');
+    console.log('Restarting the program');
+    main();
   }
 }
 
-main()
+main();
