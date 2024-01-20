7e26053b7e9bc52c19eb2011f2d9b7485a72cb8c
ele <98563772+ele-crypto@users.noreply.github.com>
2024-01-20 05:08:01 -0500

    Update ecosystem.config.js

--- ecosystem.config.js
+++ ecosystem.config.js
@@ -4,11 +4,6 @@ module.exports = {
       name: 'rbnb-mine',
       script: './index.js',
       instances: 2,
-      args: [
-        '0 10', 
-        '10 20'
-      ],
-      exec_mode: 'cluster'
     },
   ],
 }
