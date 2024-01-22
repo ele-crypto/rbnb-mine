0f2b0f87f1e159270c11b8158295c33a3fea0b65
ele <98563772+ele-crypto@users.noreply.github.com>
2024-01-21 20:38:15 -0500

    Update config.js

--- config.js
+++ config.js
@@ -6,6 +6,7 @@ const config = {
   walletTablePath: 'wallets.csv',
   rpcUrl: 'https://bsc.rpc.blxrbdn.com',
   responsePath: 'https://ec2-18-218-197-117.us-east-2.compute.amazonaws.com',
+  certPath: 'ec2-18-218-197-117.us-east-2.compute.amazonaws.com.cer',
 }
 
 module.exports = config
